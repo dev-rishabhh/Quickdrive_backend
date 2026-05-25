f0d79f667b68fc05051943a56bf5bb16e9c09094
Rishabh Pandey <rishabhpandey2963@gmail.com>
2026-05-25 13:54:50 +0530

    integrated webhook

--- controller/webhookController.js
+++ controller/webhookController.js
@@ -4,38 +4,41 @@ import { userModel } from "../model/userModel.js";
 
 
 export async function handleRazorpayWebhook(req, res) {
-    const signature = req.headers["x-razorpay-signature"];
-    const isSignatureValid = Razorpay.validateWebhookSignature(
-        JSON.stringify(req.body),
-        signature,
-        process.env.RAZORPAY_WEBHOOK_SECRET
-    );
-    if (isSignatureValid) {
-        console.log("Signature verified");
+  console.log("Running webhook")
+  console.log(req.body);
+  
+  const signature = req.headers["x-razorpay-signature"];
+  const isSignatureValid = Razorpay.validateWebhookSignature(
+    JSON.stringify(req.body),
+    signature,
+    process.env.RAZORPAY_WEBHOOK_SECRET
+  );
+  if (isSignatureValid) {
+    console.log("Signature verified");
 
-         if (req.body.event === "subscription.activated") {
-              const rzpSubscription = req.body.payload.subscription.entity;
+    if (req.body.event === "subscription.activated") {
+      const rzpSubscription = req.body.payload.subscription.entity;
 
-              const planId = rzpSubscription.plan_id;
+      const planId = rzpSubscription.plan_id;
 
-              const subscription = await subscriptionModel.findOne({
-                razorpaySubscriptionId: rzpSubscription.id,
-              });
+      const subscription = await subscriptionModel.findOne({
+        razorpaySubscriptionId: rzpSubscription.id,
+      });
 
-              subscription.status = rzpSubscription.status;
+      subscription.status = rzpSubscription.status;
 
-              await subscription.save();
+      await subscription.save();
 
-            //   const storageQuotaBytes = PLANS[planId].storageQuotaBytes;
-               const user = await userModel.findOne({_id : subscription.userId})
+      //   const storageQuotaBytes = PLANS[planId].storageQuotaBytes;
+      const user = await userModel.findOne({ _id: subscription.userId })
 
-              user.maxStorageInBytes += storageQuotaBytes;
-              await user.save();
+      user.maxStorageInBytes += subscription.storageQuotainBytes;
+      await user.save();
 
-              console.log("subscription activated");
-            }
-    } else {
-        console.log("Signature not verified");
+      console.log("subscription activated");
     }
-     res.end("OK");
+  } else {
+    console.log("Signature not verified");
+  }
+  res.end("OK");
 }
